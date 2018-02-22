8612b2bb067a03a63b7ac62d9034dd99d1debfdc
bgavran3 <bruno.gavranovic@fer.hr>
2018-02-22 09:57:01 +0100

    huge mess

--- src/backprop_functor.hs
+++ src/backprop_functor.hs
@@ -7,7 +7,7 @@ type I p a b = (p -> a -> b)
 type U p a b = (p -> a -> b -> p)
 type R p a b = (p -> a -> b -> a)
 
-ijComp :: I p a b -> I q b c -> I (p, q) a c
+ijComp :: (p -> a -> b) -> (q -> b -> c) -> ((p, q) -> a -> c)
 ijComp i j = \(p, q) a -> j q (i p a)
 
 uvComp :: U p a b -> U q b c -> b -> U (p, q) a c
@@ -26,13 +26,32 @@ data Learner p a b = Learner {
   req :: p -> a -> b -> a
 }
 
-a = Learner {impl = (*), 
+l1 = Learner {impl = (*), 
              upd = (\_ a _ -> a), 
              req = (\p _ _ -> p)}
 
-b = Learner {impl = (*), 
+l2 = Learner {impl = (*), 
              upd = (\_ a _ -> a), 
              req = (\p _ _ -> p)}
+
+instance Category (Learner g) where
+  id = Learner {impl = (\_ a -> a),
+                upd = (\p _ _ -> p),
+                req = (\_ a _ -> a)}
+
+  l2 . l1 = Learner {
+                     impl = undefined,
+                     upd = undefined,
+                     req = undefined
+                    }
+
+newtype Kleisli m a b = Kleisli { runKleisli :: a -> m b }
+
+instance Monad m => Category (Kleisli m) where
+  id = Kleisli return
+  (Kleisli f) . (Kleisli g) = Kleisli $ \x -> (f <=< g) x
+
+
 
 --learnerComp :: Learner p a b -> Learner q b c -> a -> Learner (p, q) a c
 --learnerComp (L p i u r) (L q j v s) a = L (p, q) 
@@ -41,21 +60,6 @@ b = Learner {impl = (*),
 --                                          (rsComp r s (i p a))
 
 
-newtype Kleisli m a b = Kleisli { runKleisli :: a -> m b }
-
-instance Monad m => Category (Kleisli m) where
-  id = Kleisli return
-  (Kleisli f) . (Kleisli g) = Kleisli $ \x -> (f <=< g) x
-
-instance Category (Learner g) where
-  id = Learner {impl = (\_ a -> a),
-                upd = (\p _ _ -> p),
-                req = (\_ a _ -> a)}
---  l1@(Learner _ _ _) . l2@(Learner _ _ _) = Learner {
---                                    impl = ijComp (impl l1) (impl l2),
---                                    upd = uvComp (upd l1) (upd l2) (impl l1),
---                                    req = rsComp (req l1) (req l2) (impl l1)
---                                            }
 
 --  ((L (p,i,u,r)) . (L (q,j,v,s))) a = L ((p, q) ,
 --                                        (ijComp i j),
