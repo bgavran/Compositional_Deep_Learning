c94f66d1a27953e318d033897288e7263c3eaf0b
bgavran3 <bruno.gavranovic@fer.hr>
2018-03-05 18:10:00 +0100

    added category learn

--- src/backprop_functor.hs
+++ src/backprop_functor.hs
@@ -1,70 +1,71 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
-
 import Control.Category
 import Control.Monad
 
+data MyParam p
+  = NoParam
+  | Param p
+  | ParamProd (MyParam p) (MyParam p) 
+  deriving (Eq, Show)
+
 data LearnerType p a b = Learner {
-  impl :: p -> a -> b,
-  upd :: p -> a -> b -> p,
-  req :: p -> a -> b -> a
+  param :: MyParam p,
+  impl :: MyParam p -> a -> b,
+  upd :: MyParam p -> a -> b -> MyParam p,
+  req :: MyParam p -> a -> b -> a
 }
-
-
-{- 
- composition of impl, upd and req can't be defined outside of Learner 
- since we need to define let b = ... 
--}
-learnerComp :: LearnerType q b c -> LearnerType p a b -> LearnerType (p, q) a c
-learnerComp l2 l1 = Learner {impl = \(p, q) a -> (impl l2) q ((impl l1) p a), 
-                             upd = \(p, q) a c -> let b = (impl l1) p a
-                                                  in ((upd l1) p a b, (upd l2) q b c),
-                             req = \(p, q) a c -> let b = (impl l1) p a
-                                                  in (req l1) p a ((req l2) q b c)
-}
-
-l1 = Learner {impl = \p a -> p * a, 
-             upd = \_ a _ -> a, 
-             req = \p _ _ -> p}
-
-l2 = Learner {impl = \p a -> p + a, 
-             upd = \_ a _ -> 1, 
-             req = \p _ _ -> 1}
-
-l3 = learnerComp l2 l1
-
-c = (impl l3) (2, 3) 4 -- super interesting :)
-
-
 
 instance Category (LearnerType p) where
-  id = Learner {impl = (\_ a -> a),
-                upd = (\p _ _ -> p),
-                req = (\_ a _ -> a)}
-  (.) = undefined --learnerComp 
+  id = Learner {param = undefined,
+                impl = \_ a -> a,
+                upd = \p _ _ -> p,
+                req = \_ a _ -> a}
 
- 
-class Cat obj where
-  id :: obj a -> obj a
-  (.) :: (obj b -> obj c) -> (obj a -> obj b) -> obj a -> obj c
+  (.) l2 l1 = Learner {param = ParamProd (param l1) (param l2),
+                       impl = \(ParamProd p q) a -> let b = (impl l1) p a
+                                                    in (impl l2) q b, 
+                       upd = \(ParamProd p q) a c -> let b = (impl l1) p a
+                                                     in ParamProd ((upd l1) p a b) ((upd l2) q b c),
+                       req = \(ParamProd p q) a c -> let b = (impl l1) p a
+                                                     in (req l1) p a ((req l2) q b c)}
 
-newtype Kleisli m a b = Kleisli { runKleisli :: a -> m b }
+monProd :: LearnerType p c d -> LearnerType p a b -> LearnerType p (a, c) (b, d)
+monProd l2 l1 = Learner {param = ParamProd (param l1) (param l2),
+                         impl = \(ParamProd p q) (a, c) -> ((impl l1) p a, (impl l2) q c),
+                         upd = \(ParamProd p q) (a, c) (b, d) -> ParamProd ((upd l1) p a b) ((upd l2) q c d),
+                         req = \(ParamProd p q) (a, c) (b, d) -> ((req l1) p a b, (req l2) q c d)}
 
-instance Monad m => Category (Kleisli m) where
-  id = Kleisli return
-  (Kleisli f) . (Kleisli g) = Kleisli $ \x -> (f <=< g) x
+unit = Learner {param = Param 1,
+               impl = \(Param p) _ -> p,
+               upd = \(Param p) _ b -> Param p,
+               req = \_ _ _ -> 0 }
 
+mul = Learner {param = NoParam,
+              impl = \_ (f, s) -> f + s,
+              upd = \_ _ _ -> NoParam,
+              req = \_ a b -> a}
 
+l1 = Learner {param = Param 3,
+             impl = \(Param p) a -> p * a, 
+             upd = \_ a _ -> Param a, 
+             req = \(Param p) _ _ -> p}
 
---learnerComp :: Learner p a b -> Learner q b c -> a -> Learner (p, q) a c
---learnerComp (L p i u r) (L q j v s) a = L (p, q) 
---                                          (ijComp i j)
---                                          (uvComp u v (i p a)) 
---                                          (rsComp r s (i p a))
+l2 = Learner {param = Param 5,
+             impl = \(Param p) a -> p + a, 
+             upd = \_ a _ -> Param 1, 
+             req = \p _ _ -> 1}
 
+l3 = Learner {param = NoParam,
+              impl = \_ a -> exp a,
+              upd = \_ _ _ -> NoParam,
+              req = \p a b -> b * ((impl l3) p a)}
 
+lSerial = l2 Control.Category.. l1
+lParallel = l2 `monProd` l1
 
---  ((L (p,i,u,r)) . (L (q,j,v,s))) a = L ((p, q) ,
---                                        (ijComp i j),
---                                        (uvComp u v (i p a)) ,
---                                        (rsComp r s (i p a)))
+f :: LearnerType p a b -> a -> b
+f l a = (impl l) (param l) a
 
+outSerial = f lSerial 2
+outParallel = f lParallel (2, 3)
+
+newP = (upd lSerial) (param lSerial) 2 1000
