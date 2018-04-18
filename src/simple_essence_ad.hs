dda2bfdb43223f74d6db871096f4296be7cb58d4
bgavran3 <bruno.gavranovic@fer.hr>
2018-04-18 18:06:05 +0200

    fixed stuff, added tensors and part of continuations

--- src/simple_essence_ad.hs
+++ src/simple_essence_ad.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE InstanceSigs #-}
 
+import Numeric.LinearAlgebra.Array
+import Numeric.LinearAlgebra.Array.Util
 import qualified CategoricDefinitions as Cat
 
 
@@ -22,26 +24,26 @@ instance Cat.Cartesian (->) where
 
 ----------------
 
-newtype D a b = D {eval :: a -> (b, a -> b)}
+newtype D k a b = D {eval :: a -> (b, a `k` b)}
 
-linearD :: (a -> b) -> D a b
-linearD f = D $ \a -> (f a, f)
+linearD :: (a -> b) -> (a `k` b) -> D k a b
+linearD f f' = D $ \a -> (f a, f')
 
-instance Cat.Category D where
-  id      = linearD id
+instance Cat.Category k => Cat.Category (D k) where
+  id      = linearD id Cat.id
   g . f   = D $ \a -> let (b, f') = eval f a
                           (c, g') = eval g b
-                      in (c, g' . f')
+                      in (c, g' Cat.. f')
 
-instance Cat.Monoidal D where
+instance Cat.Monoidal k => Cat.Monoidal (D k) where
   f `x` g = D $ \(a, b) -> let (c, f') = eval f a
                                (d, g') = eval g b
                            in ((c, d), f' `Cat.x` g')
 
-instance Cat.Cartesian D where
-  exl = linearD Cat.exl
-  exr = linearD Cat.exr
-  dup = linearD Cat.dup
+instance Cat.Cartesian k => Cat.Cartesian (D k) where
+  exl = linearD Cat.exl Cat.exl
+  exr = linearD Cat.exr Cat.exr
+  dup = linearD Cat.dup Cat.dup
 
 t :: Cat.Cartesian k => (a `k` c) -> (a `k` d) -> (a `k` (c, d))
 f `t` g = (f `Cat.x` g) Cat.. Cat.dup
@@ -80,13 +82,51 @@ instance Num a => Cat.NumCat (->) a where
   addC = uncurry (+)
   mulC = uncurry (*)
 
--- Missing Num constraint in the paper?
-instance Num a => Cat.NumCat D a where
-  negateC = linearD Cat.negateC
-  addC = linearD Cat.addC
-  --mulC = D $ \(a, b) -> (a * b, (Cat.scale b) `v` (Cat.scale a))
-  mulC = D $ \(a, b) -> (a * b, \(da, db) -> da + db)
+-- There seems to be too many constraints here, but it doesn't work otherwise?
+instance (Cat.Monoidal k,
+          Cat.Cocartesian k, 
+          Cat.Scalable k s, 
+          Cat.NumCat k s, 
+          Num s) => Cat.NumCat (D k) s where
+  negateC = linearD Cat.negateC Cat.negateC
+  addC = linearD Cat.addC Cat.addC
+  mulC = D $ \(a, b) -> (a * b, (Cat.scale b) `v` (Cat.scale a))
 
+{-
 f :: (Cat.NumCat k a,
       Cat.Cartesian k) => k a a
 f = Cat.mulC Cat.. (Cat.id `t` Cat.id)
+
+
+-}
+
+newtype Cont k r a b = Cont ((b `k` r) -> (a `k` r))
+
+cont :: Cat.Category k => (a `k` b) -> Cont k r a b
+cont f = Cont (Cat.. f)
+
+apply :: Cat.Category k => Cont k r a b -> (b `k` r) -> (a `k` r)
+apply (Cont f) g = f g
+
+instance Cat.Category k => Cat.Category (Cont k r) where
+  id = Cont id
+  (Cont g) . (Cont f) = Cont (f . g)
+
+-------------------
+
+ds # cs = listArray ds cs :: Array Double
+
+sh x = putStr . formatFixed 2 $ x
+
+a = [2, 3] # [1..] ! "ij"
+b = [3, 5] # [10..] ! "jk"
+
+c = a * b
+
+-- Assumes single-letter index names
+onesLike c = let d  = map iDim $ dims c
+                 ch = concat $ map iName $ dims c
+             in d # (repeat 1) ! ch
+
+aGrad = (onesLike c) * b
+bGrad = (onesLike c) * a
