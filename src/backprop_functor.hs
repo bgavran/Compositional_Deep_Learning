5bbd944fd69aa0c41d95d0f9d815d38b37ffef24
bgavran3 <bruno.gavranovic@fer.hr>
2018-04-18 21:52:41 +0200

    removed old code, refactored

--- src/backprop_functor.hs
+++ src/backprop_functor.hs
@@ -17,61 +17,32 @@ data LearnerType p a b = Learner {
 }
 
 instance Cat.Category (LearnerType p) where
-  id = Learner {param = undefined,
-                impl = \_ a -> a,
-                upd = \p _ _ -> p,
-                req = \_ a _ -> a}
+  id = Learner {param = NoP,
+                impl  = \_ a -> a,
+                upd   = \_ _ _ -> NoP,
+                req   = \_ a _ -> a}
 
   l2 . l1 = Learner {param = (param l1) `X` (param l2),
-                     impl = \(p `X` q) a -> let b = (impl l1) p a
-                                            in (impl l2) q b, 
-                     upd = \(p `X` q) a c -> let b = (impl l1) p a
-                                             in ((upd l1) p a b) `X` ((upd l2) q b c),
-                     req = \(p `X` q) a c -> let b = (impl l1) p a
-                                             in (req l1) p a ((req l2) q b c)}
+                     impl  = \(p `X` q) a   -> let b = (impl l1) p a
+                                               in (impl l2) q b, 
+                     upd   = \(p `X` q) a c -> let b = (impl l1) p a
+                                               in ((upd l1) p a b) `X` ((upd l2) q b c),
+                     req   = \(p `X` q) a c -> let b = (impl l1) p a
+                                               in (req l1) p a ((req l2) q b c)}
 
 
 instance Cat.Monoidal (LearnerType p) where
   l1 `x` l2 = Learner {param = (param l1) `X` (param l2),
-                       impl = \(p `X` q) (a, c) -> ((impl l1) p a, (impl l2) q c),
-                       upd = \(p `X` q) (a, c) (b, d) -> ((upd l1) p a b) `X` ((upd l2) q c d),
-                       req = \(p `X` q) (a, c) (b, d) -> ((req l1) p a b, (req l2) q c d)}
+                       impl  = \(p `X` q) (a, c)        -> ((impl l1) p a, (impl l2) q c),
+                       upd   = \(p `X` q) (a, c) (b, d) -> ((upd l1) p a b) `X` ((upd l2) q c d),
+                       req   = \(p `X` q) (a, c) (b, d) -> ((req l1) p a b, (req l2) q c d)}
 
-etaVal = 0.01
-
-bias = Learner {param = P 1,
-                impl = \(P p) _ -> p,
-                upd = \(P p) a b -> P $ sgd p ((f bias a) - b) etaVal,
-                req = \_ _ _ -> 0}
-
-fSigm :: Double -> Double
-fSigm x = 1 / (1 + (exp $ negate x))
-
-dfSigm :: Double -> Double
-dfSigm x = (fSigm x) * (1 - fSigm x)
-
-sigmoid = Learner {param = NoP,
-                   impl = \_ a -> fSigm a,
-                   upd = \_ _ _ -> NoP,
-                   req = \_ a b -> a - (a - b) * dfSigm a}
-
-sgd :: Double -> Double -> Double -> Double
-sgd p pGrad eta = p - eta * pGrad
-
-scalarMul = Learner {param = P 2,
-                     impl = \(P p) a -> p * a,
-                     upd = \(P p) a b -> P $ sgd p (a*((f scalarMul a) - b)) etaVal,
-                     req = \(P p) a b -> sgd a (p*((f scalarMul a) - b)) etaVal}
-
-unit = Learner {param = P 1,
-               impl = \(P p) _ -> p,
-               upd = \(P p) _ b -> P p,
-               req = \_ _ _ -> 0 }
-
-mul = Learner {param = NoP,
-              impl = \_ (f, s) -> f + s,
-              upd = \_ _ _ -> NoP,
-              req = \_ a b -> a}
+-- No instance for Num a?
+--instance Cat.Cartesian (LearnerType p) where
+--  dup = Learner {param = NoP,
+--                 impl  = \_ a -> (a, a),
+--                 upd   = \_ _ _ -> NoP,
+--                 req   = \_ a (b, c) -> b + c - a}
 
 l1 = Learner {param = P 3,
              impl = \(P p) a -> p * a, 
@@ -82,11 +53,6 @@ l2 = Learner {param = P 5,
              impl = \(P p) a -> p + a, 
              upd = \_ a _ -> P 1, 
              req = \p _ _ -> 1}
-
-l3 = Learner {param = NoP,
-              impl = \_ a -> exp a,
-              upd = \_ _ _ -> NoP,
-              req = \p a b -> b * ((impl l3) p a)}
 
 lSerial = l2 Cat.. l1
 lParallel = l2 `Cat.x` l1
