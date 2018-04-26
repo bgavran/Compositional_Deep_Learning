76872cc6e4e9908cb58a1808c7b964c1c342ce49
bgavran3 <bruno.gavranovic@fer.hr>
2018-04-26 22:35:58 +0200

    started figuring out how constraintkinds work

--- src/CategoricDefinitions.hs
+++ src/CategoricDefinitions.hs
@@ -3,29 +3,48 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DefaultSignatures #-}
 
 module CategoricDefinitions where
 
 import GHC.Exts (Constraint)
 
+class NoConstraint a where
+
+instance NoConstraint a where
+
 class Category k where
-  id  :: a `k` a
-  (.) :: (b `k` c) -> (a `k` b) -> (a `k` c)
+  type Allowed k :: * -> Constraint
+  type Allowed k = NoConstraint
+
+  id  :: Allowed k a => a `k` a
+  (.) :: (Allowed k a, 
+          Allowed k b, 
+          Allowed k c) => (b `k` c) -> (a `k` b) -> (a `k` c)
 
 class Category k => Monoidal k where
-  x :: (a `k` c) -> (b `k` d) -> ((a, b) `k` (c, d)) 
+  x :: (Allowed k a,
+        Allowed k b,
+        Allowed k c,
+        Allowed k d,
+        Allowed k (a, b),
+        Allowed k (c, d)) => (a `k` c) -> (b `k` d) -> ((a, b) `k` (c, d)) 
 
 class Monoidal k => Cartesian k where
-  exl :: (a, b) `k` a
-  exr :: (a, b) `k` b
-  dup :: a `k` (a, a)
-
--- I haven't figured out yet how ConstraintKinds work so that's why there's a Num constraint here and throughout many places in the code. 
+  exl :: (Allowed k (a, b),
+          Allowed k a) => (a, b) `k` a
+  exr :: (Allowed k (a, b),
+          Allowed k b) => (a, b) `k` b
+  dup :: (Allowed k (a, a),
+          Allowed k a) => a `k` (a, a)
 
 class Category k => Cocartesian k where
-  inl :: (Num a, Num b) => a `k` (a, b)
-  inr :: (Num a, Num b) => b `k` (a, b)
-  jam :: Num a => (a, a) `k` a
+  inl :: (Allowed k a,
+          Allowed k b) => a `k` (a, b)
+  inr :: (Allowed k a,
+          Allowed k b) => b `k` (a, b)
+  jam :: (Allowed k a,
+          Allowed k (a, a)) => (a, a) `k` a
 
 --------------------------------------
 
